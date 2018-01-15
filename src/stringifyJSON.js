4ef065986103a30246e55f8fec7bfefce52323c7
mattman240 <mattmull98@gmail.com>
2018-01-15 11:24:47 -0800

    Finished JSON.stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,52 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  var returnString = '';
+  
+  var helperFunction = function (obj) {
+    
+    if (obj === null) {
+      returnString += 'null';
+      
+    } else if (Array.isArray(obj) === true) {
+      returnString += '[';
+      obj.forEach(function (index) {
+        helperFunction(index);
+        if (obj.length > 1 && index !== obj[obj.length - 1]) {
+          returnString += ',';
+        }
+      });
+      returnString += ']';
+      
+    } else if (typeof obj === 'object') {
+      returnString += '{';
+      var objKeys = Object.keys(obj);
+      var objLength = objKeys.length;
+      for (var key in obj) {
+        if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+          returnString += '"';
+          returnString += key;
+          returnString += '":';
+          helperFunction(obj[key]);
+          if (objLength > 1 && key !== objKeys[objLength - 1]) {
+            returnString += ',';
+          }
+        }
+      }
+      returnString += '}';
+      
+    } else if (typeof obj === 'string') {
+      returnString += '"';
+      returnString += obj;
+      returnString += '"';
+      
+    } else {
+      returnString += obj;
+    }
+  };
+  
+  helperFunction(obj);
+
+  return returnString;
 };
